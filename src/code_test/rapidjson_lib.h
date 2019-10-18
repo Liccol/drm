33fe707e483628eb58ff8bf39e405c427c210113
CaranLic <740821011@qq.com>
2019-10-18 16:47:56 +0800

    add rapidjson, to be finished.

---
+++ src/code_test/rapidjson_lib.h
@@ -0,0 +1,8 @@
+
+#include "rapidjson/rapidjson.h"
+#include "rapidjson/document.h"
+#include "rapidjson/reader.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+
+using namespace rapidjson;
