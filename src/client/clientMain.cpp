5136dc624cbbc29a8d037b330e08c5a921c37955
CaranLic <740821011@qq.com>
2019-11-15 16:43:42 +0800

    添加httppost内容

--- src/client/clientMain.cpp
+++ src/client/clientMain.cpp
@@ -97,70 +97,7 @@ namespace licenseRsp {
 	void from_json(const json& j, reqForm& req) {
 	}
 }
-/*****************************************************************************
-* Function: <name> extract_json
-* Description: 从string里面截取第一个json串
-* Parameter: 输入string，截取的json
-* Returns: 截取的json
-* Others: 只截取第一个json，如完全解析，轮训即可
-*****************************************************************************/
-//string extractJson(string str_in)
-//{
-//	string str_out = "";
-//	int end_time = 1;//string中截取的}的个数，初始为1
-//	do
-//	{
-//		int _start = str_in.find("{");//第一个{的位置
-//		if (-1 == _start)
-//			return "";
-//		int _end = 0;
-//		for (int i = 0; i < end_time; i++)//第 end_time个}的位置
-//		{
-//			_end = str_in.find("}", _end);
-//			if (-1 == _end)
-//				return "";
-//			_end++;
-//		}
-//		std::string temp = str_in.substr(_start, _end - _start);//第一个{和第end_time个}之间的字符串
-//
-//		//检测temp字符串含有的{个数
-//		int start_num = 0;
-//		do
-//		{
-//			int comma_n = 0;
-//			comma_n = temp.find("{");
-//			if (-1 == comma_n)
-//				break;
-//			temp.erase(0, comma_n + 1);
-//			start_num++;
-//		} while (true);
-//
-//		//检测temp字符串含有的}个数
-//		int end_num = 0;
-//		temp = str_in.substr(_start, _end - _start);
-//		do
-//		{
-//			int comma_n = 0;
-//			comma_n = temp.find("}");
-//			if (-1 == comma_n)
-//				break;
-//			temp.erase(0, comma_n + 1);
-//			end_num++;
-//		} while (true);
-//
-//		if (start_num == end_num)//如果{和}的数量相等，则判断为标准json
-//		{
-//			str_out = str_in.substr(_start, _end - _start + 1);
-//			return str_out;
-//		}
-//		else//如不等，重新截取temp，下次多截取一个}
-//		{
-//			end_time++;
-//		}
-//
-//	} while (true);
-//
-//}
+
 int main(int argc, char argv[])
 {
 	licenseReq::reqForm licenseReq;
@@ -169,18 +106,22 @@ int main(int argc, char argv[])
 	cout << "send req: " << reqStr << endl;
 	// string testStr = "{\"name\":\"test\",\"data\":1}";
 	// http destination
-	HttpRequest httpReq("127.0.0.1", 8080);
+	// 本地测试
+	// HttpRequest httpReq("127.0.0.1", 8080);
+	// ECS测试
+	HttpRequest httpReq("101.132.174.165", 8080);
 	// http POST request, get response
 	string res = httpReq.HttpPost("/licenseReq", reqStr);
-	cout << endl<<endl<< "get response: " <<endl<< res << endl;
+	// cout << endl<<endl<< "get response: " <<endl<< res << endl;
+	// get JSON response data
 	string licenseRspStr = "";
 	int pos = res.find("{");
-	cout << pos << endl;
+	// cout << pos << endl;
 	licenseRspStr = res.substr(pos);
-	cout << licenseRspStr << endl;
-	// 貌似不是这么parse的?或者结构使用用[]来获取的
+	// cout << licenseRspStr << endl;
+	// parse JSON data
 	auto licenseRspObj = json::parse(licenseRspStr);
-	cout << "JSON parse done!" << endl;
+	// cout << "JSON parse done!" << endl;
 	cin.get();
 	if (licenseRspObj["version"] == "2.0" && licenseRspObj["type"] == "licenseResponse")
 	{
